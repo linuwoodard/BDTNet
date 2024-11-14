321d02afbfd5590d80d3b250564ac59cff1c248d
Linus Woodard <lwoodard@stanford.edu>
2024-11-13 20:34:34 -0800

    add show_predicted_Qs to utils.  cleaned CNN notebook

--- optcial_Q Net/utils.py
+++ optcial_Q Net/utils.py
@@ -57,3 +57,47 @@ def read_json(path):
     with open(path, 'r') as json_file:
         data = json.load(json_file)
     return data
+
+
+def show_predicted_Qs(example_numbers, batch_number, test_dataset, model):
+    """
+    given tensorflow dataset, model, and example numbers, this function prints the predicted Qs for the examples
+    as well as the Qs from the dataset.
+
+    Inputs:
+        example_numbers: list of example numbers to be predicted
+        batch_number: batch number to be predicted
+        test_dataset: tensorflow dataset -- typically test dataset
+        model: trained model
+
+    Outputs:
+    """
+
+    train_numbers = example_numbers
+
+
+    test_iterator = iter(test_dataset)
+    for batch in range(batch_number):
+        # itterate to the batch of interest
+        batch_X, batch_Y = next(test_iterator)
+        
+
+    predicted = []
+    true = []
+    for train_number in train_numbers:
+        # Extract a single input and label pair from the batch
+        # For example, take the first item from the batch
+        single_input = batch_X[train_number].numpy()  # Convert to NumPy array
+        single_label = batch_Y[train_number].numpy()  # Convert to NumPy array
+        
+        single_input =  np.expand_dims(single_input, axis=0) 
+        
+        prediction = model.predict(single_input, verbose = 0)
+
+        predicted.append(10**(prediction[0][0]))
+        true.append(10**(single_label[0]))
+
+        print("predicted Q = {}".format(10**(prediction[0][0])))
+        print("true Q = {}\n".format(10**(single_label[0])))
+
+    return predicted, true
